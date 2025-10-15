841b76394fb120f18c7cc0622a33d09226081848
balach124 <balach12345q@gmail.com>
2025-10-15 19:12:18 +0500

    files added

---
+++ 2 Assigment/script.js
@@ -0,0 +1,27 @@
+// chapter 4
+
+
+//legal Words
+
+
+var _city = "karachi"
+var $Name = "Owais-Chomu"
+var last_name = "Owais-Pendu"
+var email = "owais-aloo@gmail.com"
+var ph0ne = "Nhi Bataonga Lala"
+
+console.table([_city,$Name,last_name,email,ph0ne]);
+
+
+// illegal words
+
+// var var = "keywords use nhi hota";
+// var -name = "dash allow nhi he"
+// var 1email = "Number se Start Nhi Hota"
+// var last name = "space allow nhi Hota"
+// var alert = "JS ka build In Words he Allow nhi hoga"
+
+
+// chapter 5
+var result = 200 + 400 + 567 - 234 * 798 * 2359 / 230 / 240;
+console.log( "200 + 400 + 567 - 234 X 798 X 2359 ÷ 230 ÷ 240 --->" , result );
