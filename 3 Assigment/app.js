64ba51554c2725955fbd6e50bc3cba6fac247ae4
balach12345q <balach12345q@gmail.com>
2025-10-29 17:39:26 +0500

    file added

---
+++ 3 Assigment/app.js
@@ -0,0 +1,68 @@
+let uesername = prompt("Sab log apne \n identity karwahe") //chapter 9
+
+// chapter6
+let num = "8"
+++ num
+num--
+document.writeln("<h1>Chapter 6</h1><h1>Question No 1</h1>"+ " The Answer is-->" + num );
+
+
+
+var add = "1"
+++add
+document.writeln("<h1>Question No 2</h1>The equal is-->"+add)
+
+var add =  "5"
+--add
+document.writeln("<h1>Question No 3</h1>The equal is--> "+add);
+
+
+var add1 = "1"
+var add1 ="5"
+-- add1
+document.writeln( "<h1>Question No 4</h1> the equal is "+add1 + '');
+
+
+// chapter 7
+
+var totalcast = 8*2-5
+var totalcast1 =( 4/8)+7
+
+var result =totalcast + totalcast1
+document.writeln("<h1>Chapter 7</h1><h1>Question No 1</h1>The equal is-->"+ result);
+
+
+
+
+
+var multiple =(4*6)*6;
+var multiples =(3*6)*3;
+
+var multiply = multiple + multiples;
+document.writeln("<h1>Question No 2</h1>The equal is-->"+ multiply);
+
+// chapter 8
+ 
+
+let firstName = "Onais";
+let lastname = "Baloch";
+
+let fullname = firstName +' ' + lastname;
+document.writeln("<h1>Chapter No 8</h1><h1>Question No 1</h1>The Name is-->"+ fullname);
+
+let place = "Balochistan";
+let nature ="Gwadar"
+
+let country = place + ' ' + nature;
+document.writeln("<h1>Question No 2</h1>The Province Name is-->" + country);
+
+
+
+
+
+
+
+
+
+
+
