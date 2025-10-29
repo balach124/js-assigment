3c33c28b89ccb0ec151b3eebe80064cb6de0126b
balach12345q <balach12345q@gmail.com>
2025-10-29 17:18:41 +0500

    file added

---
+++ 1 Assigment/script.js
@@ -0,0 +1,23 @@
+ //  chapter 1
+ alert("Hello I'M Balach")
+ alert("A Editor And Frontend Developer")
+ alert("Wanted To Make My Future Bright")
+ document.writeln("hlo world")
+
+
+// chapter 2
+
+const username ="Owais"
+let email ="owais-londu@gmail.com"
+var password ="owais12345"
+City ="Karachi"
+console.table([username , email ,password ,City])
+
+// chapter 3
+
+let num1 = 1000
+let num2 = -90
+let num3 = 467
+let num4 = 654
+let result = num1 - num2 * num3 + num4
+console.log(result)
