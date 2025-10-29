b9de1d962a5359c4e915238dcc06f08dcc6b933f
balach12345q <balach12345q@gmail.com>
2025-10-29 17:15:15 +0500

    file added

---
+++ 2 Assigment/app.js
@@ -0,0 +1,28 @@
+var sum = "8"
+var sum1 = "9"
+
+var equal = (8 + 9)
+
+console.log("the equal is-->" +equal)
+
+
+
+
+console.log(500-956)
+
+
+var divide = "15"
+var divide = "8"
+
+console.log(divide/divide)
+
+console.log(15/10)
+
+
+console.log(10%4)
+
+console.log(5/0)
+console.log(-5/0)
+
+console.log(100*45)
+
