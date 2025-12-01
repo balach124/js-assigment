a12bd043822fb03ed33833e15ce6715198caf764
balach124 <balach12345q@gmail.com>
2025-12-01 18:29:00 +0500

    main

---
+++ Facebook-Assigment/script.js
@@ -0,0 +1,7 @@
+function login() {
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("pass").value;
+
+    console.log("Email:", email);
+    console.log("Password:", pass);
+}
