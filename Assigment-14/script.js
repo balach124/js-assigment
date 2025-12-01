06bde4dea2cc7ee1073e14d2bf93778e1192fcc2
balach124 <balach12345q@gmail.com>
2025-12-01 18:16:26 +0500

    main

---
+++ Assigment-14/script.js
@@ -0,0 +1,41 @@
+////////////////
+//            //
+//   Q1 - Q4  //
+//            //
+////////////////
+
+
+
+// Q1
+function link() {
+    alert("Its Work")
+}
+
+// Q2
+function iphone() {
+    alert ("iphone mobile")
+}
+
+function samsung() {
+    alert ("samsung mobile")
+}
+
+function vivo() {
+    alert ("vivo mobile")
+}
+
+function hawa() {
+    alert ("huawei mobile")
+}
+
+// Q3
+
+function deleteRow(button) {
+        button.parentElement.parentElement.remove();
+    }
+
+
+
+// Q4
+
+
